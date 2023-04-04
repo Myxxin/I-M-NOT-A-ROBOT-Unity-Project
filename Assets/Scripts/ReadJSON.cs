9e4f1733e73c565b80e7280597bf0666cac792ca
marco-braune <marco.s.braune@filmuniversitaet.de>
2023-04-04 14:38:54 +0200

    add JSON reading and Typewriter Effect

--- Assets/Scripts/ReadJSON.cs
+++ Assets/Scripts/ReadJSON.cs
@@ -8,6 +8,7 @@ public class ReadJSON : MonoBehaviour
 {
     public TextAsset textJSON;
     public TextMeshProUGUI textElement;
+    public float textAnimSpeed = 0.04f;
     
     [System.Serializable]
 
@@ -26,12 +27,25 @@ public class ReadJSON : MonoBehaviour
     void Start()
     {
         rMessage = JsonUtility.FromJson<ReceivedMessage>(textJSON.text);
-        textElement.text = rMessage.message;
+        //textElement.text = rMessage.message;
+        StartCoroutine(TypeWrite(rMessage.message));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //new 
+    private IEnumerator TypeWrite(string displayText)
+    {
+        textElement.text = "";
+
+        foreach(char letter in displayText.ToCharArray())
+        {
+            textElement.text += letter;
+            yield return new WaitForSeconds(textAnimSpeed);
+        }
     }
 }
